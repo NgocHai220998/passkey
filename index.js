048f5f49c8239ac95a0054f85edfcd5b5b1551ec
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2023-05-21 15:05:22 +0900

    Add config

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve the apple-app-site-association file from the .well-known directory
+app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.get('/apple-app-site-association', (req, res) => {
+  // Replace the placeholders with your own app details
+  const appleAppSiteAssociation = {
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: 'TEAMID.BUNDLEID',
+          paths: ['/path/to/content'],
+        },
+      ],
+    },
+  };
+
+  // Set the appropriate headers
+  res.set('Content-Type', 'application/json');
+  res.status(200).json(appleAppSiteAssociation);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
